1f731f1178970bbfc57aabbf1edb7f735647a44a
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-03-12 10:04:57 +0500

    Created different pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-export default function App(){
+import Home from "./Pages/Home"
+import Services from "./Pages/Services"
+import Products from "./Pages/Products"
+import AboutUs from "./Pages/AboutUs"
+import ContactUs from "./Pages/Contact"
+import Career from "./Pages/Career"
+
+export default function App() {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/services" element={<Services/>}></Route>
+      <Route path="/products" element={<Products/>}></Route>
+      <Route path="/about" element={<AboutUs/>}></Route>
+      <Route path="/contact" element={<ContactUs/>}></Route>
+      <Route path="/career" element={<Career/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
