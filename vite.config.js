8a00bd4b5353a89ee358a1b5c4d5e7633faa2c0f
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-03-13 15:02:31 +0500

    updated configuration file.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react(),
   ],
+  base: "https://github.com/Bilal-19/curemed-pharmacy"
 })
