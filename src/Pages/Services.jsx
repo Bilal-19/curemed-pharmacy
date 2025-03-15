8316955765af01f31483d5cfc250f792e4629aa6
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-03-15 13:41:20 +0500

    Created service page template.

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,7 +1,18 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import HeaderCTA from "../Components/HeaderCTA";
+
 export default function Services() {
     return (
         <>
-            <p>Services</p>
+            <HeaderCTA />
+            <Header />
+
+            <div class="w-full">
+                services
+            </div>
+
+            <Footer/>
         </>
     )
 }
