f47a599479db5a3c46c8f89a749393195fde3abb
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-03-15 15:52:45 +0500

    Created template for About us page.

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,7 +1,25 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import HeaderCTA from "../Components/HeaderCTA";
+
 export default function AboutUs() {
     return (
         <>
-            <p>AboutUs</p>
+        <HeaderCTA/>
+        <Header/>
+
+        <div class="w-full bg-[url(/curemed-pharmacy/about-bg.png)] h-[333px] bg-cover bg-no-repeat">
+                <div class="grid md:grid-cols-2">
+                    <div className="m-12 md:mt-12">
+                        <h1 className="text-3xl md:text-6xl text-white font-medium ff-poppins">About Us</h1>
+                        <p className="text-white text-lg mt-5 mb-0 pb-0 text-base md:text-[18px]">
+                        At CureMed, we believe in the power of science and innovation to improve health and well-being. With a dedication to quality, safety, and affordability, we strive to provide cutting-edge pharmaceutical solutions that meet global healthcare standards.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        <Footer/>
         </>
     )
 }
